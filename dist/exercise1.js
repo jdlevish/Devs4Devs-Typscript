6ff5c4535fe8139ce8d9a3518409ae5e1c8f64f8
jdlevish <45375460+jdlevish@users.noreply.github.com>
2022-09-15 00:26:47 -0700

    began working on initial TypeScript exercises for talk

---
+++ dist/exercise1.js
@@ -0,0 +1,7 @@
+"use strict";
+let a = 1;
+let b = 3;
+const add2numbers = function (a, b) {
+    return a + b;
+};
+add2numbers(a, b);
---
+++ exercise1.ts
@@ -0,0 +1,9 @@
+
+let a = 1;
+let b =3;
+
+ export const add2numbers = function (a:number, b:number) {
+    return a + b
+ }
+
+ add2numbers(a,b);
