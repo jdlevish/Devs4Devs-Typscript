c3a1c74b8f1f70352933aada3106d577e8b0ff0d
jdlevish <45375460+jdlevish@users.noreply.github.com>
2022-09-21 16:50:35 -0700

    added typed return value example to exercise1b

--- src/exercise1b.ts
+++ src/exercise1b.ts
@@ -1,12 +1,12 @@
 
-let a = 3;
-let b ="1";
+// let a = 3;
+// let b ="1";
 
- export const add2numbers = function (a:number, b:number) {
-    console.log(a + b)
- }
+//  export const add2numbers = function (a:number, b:number) {
+//     console.log(a + b)
+//  }
 
- add2numbers(a,b);
+//  add2numbers(a,b);
 
 //  example without errors
 // let a = 1;
@@ -18,4 +18,14 @@ let b ="1";
 
 //  add2numbers(a,b);
 
+// here we define the type of the return value of the function
+
+// let a = 1;
+// let b =3;
+
+//  export const add2numbers = function (a:number, b:number):number {
+//  return a + b
+//  }
+
+//  add2numbers(a,b);
  
